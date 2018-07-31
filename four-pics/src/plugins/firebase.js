095473cb0de228c5270b98ee12c68f6b7d9e550c
Rey Anthony Renacia <reyanthonyrenacia@gmail.com>
2018-07-31 19:01:20 +0800

    Update firebase.js

--- four-pics/src/plugins/firebase.js
+++ four-pics/src/plugins/firebase.js
@@ -2,12 +2,7 @@ import * as firebase from 'firebase'
 
 // Initialize Firebase
 var config = {
-   apiKey: "AIzaSyDHoE64V6KRSyinTb4A5i5IjQ83WsIYshA",
-   authDomain: "for-dana.firebaseapp.com",
-   databaseURL: "https://for-dana.firebaseio.com",
-   projectId: "for-dana",
-   storageBucket: "",
-   messagingSenderId: "948378395843"
+   // your firebase config here
  };
   firebase.initializeApp(config);
 // leave the export, even if you don't use it
